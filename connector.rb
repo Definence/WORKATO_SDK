2f8fe70e03152b781acc927e26496142134ad989
definence <dp210592ybo@gmail.com>
2021-10-14 13:45:54 +0300

    Add jira connector

--- connector.rb
+++ connector.rb
@@ -1,22 +1,85 @@
+# require 'byebug'
+
 {
-  title: 'Connector.Rb',
+  title: "Jira SDK Lab",
 
   connection: {
+    fields: [
+      {
+        name: 'subdomain',
+        control_type: 'subdomain',
+        url: '.atlassian.net',
+        hint: 'Your Atlassian URL for your Jira Cloud instance.'
+      }, {
+        name: 'username',
+        optional: false,
+        hint: 'Your email.'
+      }, {
+        name: 'api_token',
+        control_type: 'password',
+        label: 'API token'
+      },
+    ],
+
     authorization: {
-      type: 'custom_auth'
+      type: "basic_auth",
+      apply: lambda do |connection|
+        user(connection['username'])
+        password(connection['api_token'])
+      end
     },
 
     base_uri: lambda do |connection|
-      ''
+      "https://#{connection['subdomain']}.atlassian.net"
     end
   },
 
-  test: lambda do |connection|
-
+  test: lambda do |_connection|
+    get("/rest/api/3/myself").
+      after_error_response(401) do |code, body, header, message|
+        error("#{message}: #{body}")
+      end
   end,
 
+  # https://docs.workato.com/developing-connectors/sdk/sdk-reference/actions.html#structure
   actions: {
+    get_issue: {
+      title: 'Get issue from Jira',
+      subtitle: 'Get issue details from Jira Cloud',
+      description: "Get <span class='provider'>issue ID and issue summary</span> " \
+                   "from <span class='provider'>Jira Cloud</span>",
+      help: "This action retriieves your issue ID and issue summary from Jira Cloud. Use thus acttion" \
+            " to search for issues from your Jira Cloud instance",
 
+
+      input_fields: lambda do |object_definitions, connection, config_fields|
+        [
+          {
+            name: 'issue_id',
+            label: 'Issue ID',
+            optional: false,
+          },
+        ]
+      end,
+
+      output_fields: lambda do |object_definitions|
+        [
+          { name: 'key', label: 'Issue key' },
+          { name: 'id', label: 'Issue ID' },
+          { name: 'fields', label: 'Issue fields', type: 'object', properties: [
+            { name: 'summary', label: 'Summary' },
+          ] },
+        ]
+      end,
+
+      execute: lambda do |_connection, input, _input_schema, _output_schema|
+        url = "/rest/api/3/issue/#{input['issue_id']}"
+        get(url).
+          after_error_response([404]) do |code, body, header, message|
+            error("#{message}: #{body}")
+          end
+      end,
+    },
   },
 
   triggers: {
